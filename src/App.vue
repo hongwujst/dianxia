0626098882e5b943f762a5e32459500138c2d2f4
jiangshitao <jiangshitao@boco.com.cn>
2019-11-28 13:42:53 +0800

    修改路由和内容

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,30 @@
     <div class="app-content">
       <div class="app-menu">
         <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+          <router-link to="/">
           <el-menu-item  index="1">
             <i class="el-icon-document"></i>
-            <span slot="title"><router-link to="/">招投标申请</router-link></span>
+            <span slot="title">招投标申请</span>
           </el-menu-item >
+          </router-link>
+          <router-link to="/VolBidManage">
           <el-menu-item index="2">
             <i class="el-icon-menu"></i>
-            <span slot="title"><router-link to="/VolBidManage">招投标管理</router-link></span>
+            <span slot="title">招投标管理</span>
           </el-menu-item>
+          </router-link>
+          <router-link to="/VolBidCommission">
           <el-menu-item index="3">
             <i class="el-icon-document"></i>
-            <span slot="title"><router-link to="/VolBidCommission">待办列表</router-link></span>
+            <span slot="title">待办列表</span>
           </el-menu-item>
+          </router-link>
+          <router-link to="/VolBidRecheck">
           <el-menu-item index="4">
             <i class="el-icon-setting"></i>
-            <span slot="title"><router-link to="/VolBidRecheck">价格复核</router-link></span>
+            <span slot="title">价格复核</span>
           </el-menu-item>
+          </router-link>
         </el-menu>
       </div>
       <div class="app-view">
@@ -95,12 +103,16 @@ export default {
     color:#fff;
     margin:0 10px;
   }
+  .app-view{
+    flex:1 1 auto;
+  }
   #app {
     position: absolute;
     left:0;
     top:0;
     width: 100%;
     height: 100%;
+    background: #F0F0F0;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     color: #2c3e50;
   }
